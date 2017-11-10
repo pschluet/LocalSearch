04b0655bb46cbaf41e47807b08fcd0fa1bfc20c5
Paul Schlueter <paul@paulschlueter.com>
2017-11-10 00:24:07 -0600

    Modified Run All Input Params

--- run_all.py
+++ run_all.py
@@ -5,13 +5,14 @@ from subprocess import call
 if __name__ == "__main__":
     data_files = [os.path.basename(x) for x in glob("Data/*")]
     algorithms = ["LS1", "LS2"]
-    num_random_seeds = 2
+    num_random_seeds = 10
+    time_limit_sec = 600
     classPath = "Main.jar:lib/commons-cli-1.4.jar:lib/jgrapht-core-1.0.1.jar"
 
     for file in data_files:
         for algorithm in algorithms:
             for seed in range(1,num_random_seeds + 1):
-                args = "-inst {} -alg {} -time {} -seed {}".format(file, algorithm, 2, seed)
+                args = "-inst {} -alg {} -time {} -seed {}".format(file, algorithm, time_limit_sec, seed)
                 cmd = "java -cp {} com.ps.Main {}".format(classPath, args)
 
                 print("Running {}".format(args))
