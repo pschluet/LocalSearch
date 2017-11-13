161e02ec6d348d41f13e07ebf6aed1b4de5d0481
Paul Schlueter <paul@paulschlueter.com>
2017-11-12 19:55:30 -0700

    Fixed Variable Overwrite Error

--- run_all.py
+++ run_all.py
@@ -10,25 +10,29 @@ if __name__ == "__main__":
     classPath = "Main.jar:lib/commons-cli-1.4.jar:lib/jgrapht-core-1.0.1.jar"
     file_name = "out.txt"
 
-    for file in data_files:
+    if os.path.isfile(file_name):
+        os.remove(file_name)
+
+    for data_file in data_files:
         for algorithm in algorithms:
             for seed in range(1,num_random_seeds + 1):
-                args = "-inst {} -alg {} -time {} -seed {}".format(file, algorithm, time_limit_sec, seed)
+                args = "-inst {} -alg {} -time {} -seed {}".format(data_file, algorithm, time_limit_sec, seed)
                 cmd = "java -cp {} com.ps.Main {}".format(classPath, args)
 
                 txt = "Running {}".format(args)
-                with open(file_name, "w") as file:
+                with open(file_name, "a") as file:
                     file.write(txt + "\n")
                 print(txt)
 
-                returnCode = call(cmd, shell=True, cwd="./Code/")
+                returnCode = 0 #call(cmd, shell=True, cwd="./Code/")
+                print(cmd)
 
                 if returnCode != 0:
                     txt = "*"*80 +"\nError on " + args + "\n" + "*"*80
-                    with open(file_name, "w") as file:
+                    with open(file_name, "a") as file:
                         file.write(txt + "\n")
                     print(txt)
     txt = "Done!"
-    with open(file_name, "w") as file:
+    with open(file_name, "a") as file:
         file.write(txt + "\n")
     print(txt)
