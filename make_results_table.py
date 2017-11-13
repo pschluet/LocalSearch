55a55aa69d9ca0654ed7897e5e7cc1125e87c518
Paul Schlueter <paul@paulschlueter.com>
2017-11-13 17:51:59 -0600

    Code for Results Table Creation Complete

--- make_results_table.py
+++ make_results_table.py
@@ -1,4 +1,6 @@
 import textwrap
+from glob import glob
+import numpy as np
 
 class LatexTableBuilder:
     HEADER = textwrap.dedent(r"""
@@ -21,17 +23,105 @@ class LatexTableBuilder:
         self.rows = []
         pass
 
-    def add_row(self, data_file_name, alg1_time_sec, alg1_vc_size, alg1_rel_err, alg2_time_sec, alg2_vc_size, alg2_rel_err):
-        self.rows.append(r"{}    & {:.2f}   & {}      & {:.2f}   & {:.2f}   & {}      & {:.2f}   \\ \hline"
-                         .format(data_file_name, alg1_time_sec, alg1_vc_size, alg1_rel_err, alg2_time_sec, alg2_vc_size, alg2_rel_err))
+    def add_row(self, data):
+        format_str = r"{}    & {:.2f}   & {:.0f}      & {:.2f}   & {:.2f}   & {:.0f}      & {:.2f}   \\ \hline"
+        if not data.alg1_rel_err:
+            format_str = r"{}    & {}   & {}      & {}   & {:.2f}   & {:.0f}      & {:.2f}   \\ \hline"
+        if not data.alg2_rel_err:
+            format_str = r"{}    & {:.2f}   & {:.0f}      & {:.2f}   & {}   & {}      & {}   \\ \hline"
+        if not data.alg1_rel_err and not data.alg2_rel_err:
+            format_str = r"{}    & {}   & {}      & {}   & {}   & {}      & {}   \\ \hline"
+
+        self.rows.append(format_str
+                         .format(data.instance_name.replace("_"," "),
+                                 xstr(data.alg1_time_sec),
+                                 xstr(data.alg1_vc_size),
+                                 xstr(data.alg1_rel_err),
+                                 xstr(data.alg2_time_sec),
+                                 xstr(data.alg2_vc_size),
+                                 xstr(data.alg2_rel_err)))
+
+    def add_rows(self, data_rows):
+        for row in data_rows:
+            self.add_row(row)
 
     def gen_table(self):
         return self.HEADER + "\n".join(self.rows) + self.FOOTER
 
+def xstr(input):
+    return "" if input is None else input
+
+class DataLoader:
+    def __init__(self, instance_name, optimum_vc_size):
+        self.instance_name = instance_name
+        self.opt_vc_size = optimum_vc_size
+
+        self.alg1_time_sec = None
+        self.alg1_vc_size = None
+        self.alg1_rel_err = None
+
+        self.alg2_time_sec = None
+        self.alg2_vc_size = None
+        self.alg2_rel_err = None
+
+        alg1_data = self.__load_data("LS1")
+        if not alg1_data:
+            return
+
+        alg2_data = self.__load_data("LS2")
+        if not alg2_data:
+            return
+
+        self.alg1_time_sec = alg1_data[0]
+        self.alg1_vc_size = alg1_data[1]
+        self.alg1_rel_err = alg1_data[2]
+
+        self.alg2_time_sec = alg2_data[0]
+        self.alg2_vc_size = alg2_data[1]
+        self.alg2_rel_err = alg2_data[2]
+
+    def __load_data(self, algorithm):
+        file_names = glob("output/*" + self.instance_name + "*" + algorithm + "*.trace")
+
+        if not file_names:
+            return
+
+        data = []
+        for file_name in file_names:
+            new_data = np.loadtxt(file_name, delimiter=",")
+            data.append(new_data[-1,:])
+        data = np.array(data)
+
+        # Take averages and compute relative sizes
+        time_sec = data[:,0].mean()
+        vc_size = data[:,1].mean()
+        rel_err = (vc_size - self.opt_vc_size) / self.opt_vc_size
+
+        return (time_sec, vc_size, rel_err)
+
+OPT_VALS = [
+    ('jazz', 158),
+    ('karate', 14),
+    ('football', 94),
+    ('as-22july06', 3303),
+    ('hep-th', 3926),
+    ('star', 6902),
+    ('star2', 4542),
+    ('netscience', 899),
+    ('email', 594),
+    ('delaunay_n10', 703),
+    ('power', 2203)
+]
+
+def get_all_data():
+    latexBuilder = LatexTableBuilder()
+    OPT_VALS.sort(key=lambda x: x[0])
+    for opt in OPT_VALS:
+        data = DataLoader(opt[0], opt[1])
+        latexBuilder.add_row(data)
+    return latexBuilder
+
 
 if __name__ == "__main__":
-    rtb = LatexTableBuilder()
-    rtb.add_row("set 1", 100.23402304, 2323, 0.2353242, 220.234023, 3524, 0.834242)
-    rtb.add_row("set 2", 123.2134234, 2342, 0.898234, 234.234234, 23432, 0.62342)
-
-    print(rtb.gen_table())
+    builder = get_all_data()
+    print(builder.gen_table())
