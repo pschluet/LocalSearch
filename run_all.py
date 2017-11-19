e54d256df5ad20311469b9e7f34d726e97b5e994
Paul Schlueter <paul@paulschlueter.com>
2017-11-19 07:59:12 -0600

    Increased Run Numbers for Plot Instances

--- run_all.py
+++ run_all.py
@@ -5,7 +5,8 @@ from subprocess import call
 if __name__ == "__main__":
     data_files = [os.path.basename(x) for x in glob("Data/*")]
     algorithms = ["LS1", "LS2"]
-    num_random_seeds = 10
+    num_random_seeds_min = 10
+    num_random_seeds_max = 100
     time_limit_sec = 600
     classPath = "Main.jar:lib/commons-cli-1.4.jar:lib/jgrapht-core-1.0.1.jar"
     file_name = "out.txt"
@@ -14,6 +15,10 @@ if __name__ == "__main__":
         os.remove(file_name)
 
     for data_file in data_files:
+        if 'star2' in data_file or 'power' in data_file:
+            num_random_seeds = num_random_seeds_max
+        else:
+            num_random_seeds = num_random_seeds_min
         for algorithm in algorithms:
             for seed in range(1,num_random_seeds + 1):
                 args = "-inst {} -alg {} -time {} -seed {}".format(data_file, algorithm, time_limit_sec, seed)
